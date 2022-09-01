b55c1cbd98c7b7658d18df2ace5866a4241e7f46
TRKZI Omar <81875005+Trkzi-Omar@users.noreply.github.com>
2022-09-01 22:45:59 +0100

    Containers structure

---
+++ app-root/components/Home.tsx
@@ -0,0 +1,11 @@
+import type {NextPage} from 'next'
+
+const Home: NextPage = () => {
+    return (
+        <div className={'w-9/12 h-full m-0 p-0 bg-gray-800'}>
+            Home
+        </div>
+    )
+}
+
+export default Home
---
+++ app-root/components/LeftNavBar.tsx
@@ -0,0 +1,11 @@
+import type {NextPage} from 'next'
+
+const LeftNavBar: NextPage = () => {
+    return (
+        <div className={'w-3/12 h-full m-0 p-0 bg-black-900'}>
+            MUSLIM SHOW
+        </div>
+    )
+}
+
+export default LeftNavBar
