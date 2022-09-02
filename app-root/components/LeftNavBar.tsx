f04b382195dd9fb1d547f4bb0a4c615955883f12
TRKZI Omar <81875005+Trkzi-Omar@users.noreply.github.com>
2022-09-03 00:04:58 +0100

    Configured the overall layout

--- app-root/components/LeftNavBar.tsx
+++ app-root/components/LeftNavBar.tsx
@@ -6,17 +6,17 @@ interface Props {
 
 const LeftNavBar: NextPage<Props> = ({mainHeight}) => {
     return (
-        <div className={'w-2/9 m-0 p-0 bg-gray-600 px-3 flex flex-col text-center'}>
+        <div className={'w-2/9 m-0 bg-gray-600 px-[1.15%] flex flex-col text-center'}>
             <div className={'w-full block py-3'}>
                 <div className={'w-1/6 inline-block'}>|||</div>
-                <div className={'inline-block'}>HOO POW</div>
+                <div className={'inline-block border-'}>HOO POW</div>
             </div>
-            <div className={"w-full block pt-7 pb-12 px-12"}>
+            <div className={"w-full block px-[2.8%] bg-[#51aabc] rounded"}>
                 <span className={'block whitespace-nowrap'} style={{
                     fontSize: (mainHeight * 0.02).toString() + 'px',
                     lineHeight: (mainHeight * 0.03).toString() + 'px'
                 }}>
-                    Disponible gratuitement:
+                    Disponible gratuitement :
                 </span>
                 <span className={'block font-bold'}
                       style={{
